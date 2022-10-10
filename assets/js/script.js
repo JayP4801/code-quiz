074744dcb965d396b0e65f7a1ac4d7307e0e8992
JayP4801 <jpham4801@gmail.com>
2022-10-10 16:39:02 -0500

    added buttons and centered text

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var questionIdCounter = 0;
+var pageContentEl = document.querySelector("#answer-options")
+var score = [];
+var questions = [];
+
+var questionHandlerEl = function(event){
+    event.preventDefault();
+};
+
+var generateQuestionEl = function(){
+
+};
+
+var generateAnswersEl = function(){
+    
+};
